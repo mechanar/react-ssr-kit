02751cbc2d0e0518370779bf90f6f37723a9187f
Ivan Plahteev <varus73@gmail.com>
2019-10-15 15:47:59 +0400

    lint fix

--- webpack.client.js
+++ webpack.client.js
@@ -1,16 +1,16 @@
-const path = require("path");
-const baseConfig = require("./webpack.base");
-const merge = require("webpack-merge");
+const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
 
 const config = {
-	mode: "development",
-	entry: "./src/client/index.js",
+  mode: 'development',
+  entry: './src/client/index.js',
 
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "public")
-	},
-	devtool: "inline-source-map"
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devtool: 'inline-source-map'
 };
 
 module.exports = merge(baseConfig, config);
--- webpack.server.js
+++ webpack.server.js
@@ -1,17 +1,17 @@
-const path = require("path");
-const baseConfig = require("./webpack.base");
-const webpackNodeExternals = require("webpack-node-externals");
-const merge = require("webpack-merge");
+const path = require('path');
+const webpackNodeExternals = require('webpack-node-externals');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
 
 const config = {
-	target: "node",
-	mode: "production",
-	entry: "./src/server.js",
-	externals: [webpackNodeExternals()],
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "build")
-	}
+  target: 'node',
+  mode: 'production',
+  entry: './src/server.js',
+  externals: [webpackNodeExternals()],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  }
 };
 
 module.exports = merge(baseConfig, config);
